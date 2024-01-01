4ad584512434e98d20e39e7759f18453a0d8225a
Raheem Kawojue <kawojue08@gmail.com>
2024-01-01 02:46:40 +0100

    [add] funcs

---
+++ gobcrypt.go
@@ -0,0 +1,22 @@
+package gobcrypt
+
+import "golang.org/x/crypto/bcrypt"
+
+// It takes the password you want to hash.
+// Note: it uses default cost.
+func HashPassword(password string) string {
+	hashedPswd, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return string(hashedPswd)
+}
+
+// It takes the already hashed password first then the password you want to compare it with.
+func VerifyPassword(hashedPassword string, password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+
+	return err == nil
+}
