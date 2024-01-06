52fe682c25cef66f4e4e00bf7526e69b57fe641a
Raheem Kawojue <kawojue08@gmail.com>
2024-01-06 01:43:24 +0100

    refacotored

--- gobcrypt.go
+++ gobcrypt.go
@@ -1,22 +1,40 @@
+// Package gobcrypt provides password hashing and comparison using golang.org/x/crypto/bcrypt.
 package gobcrypt
 
 import "golang.org/x/crypto/bcrypt"
 
-// It takes the password you want to hash.
-// Note: it uses default cost.
+// HashPassword generates a bcrypt hash for the given password using the default cost.
+// It returns the hashed password as a string.
 func HashPassword(password string) string {
-	hashedPswd, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
 		panic(err)
 	}
-
-	return string(hashedPswd)
+	return string(hashedPassword)
 }
 
-// It takes the already hashed password first then the password you want to compare it with.
-func VerifyPassword(hashedPassword string, password string) bool {
+// CompareHashAndPassword compares a hashed password with a plaintext password.
+// It takes the already hashed password first, followed by the plaintext password.
+// It returns true if the passwords match, otherwise false.
+func CompareHashAndPassword(hashedPassword, password string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
-
 	return err == nil
+}
+
+// GenerateFromPassword is a wrapper for bcrypt.GenerateFromPassword with a custom cost.
+// It allows you to specify a cost for password hashing.
+// It returns the hashed password as a string.
+func GenerateFromPassword(password string, cost int) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), cost)
+	if err != nil {
+		return "", err
+	}
+	return string(hashedPassword), nil
+}
+
+// Cost returns the cost used in the provided hashed password.
+// It can be used to check the cost of an existing hash.
+func Cost(hashedPassword string) (int, error) {
+	cost, err := bcrypt.Cost([]byte(hashedPassword))
+	return cost, err
 }
